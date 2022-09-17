61bde57e99bab8c47e6b850846949fd129595439
Cristian Rojas <crisdarroca@hotmail.com>
2022-09-17 13:59:06 -0500

    public

---
+++ src/02-methods.ts
@@ -0,0 +1,32 @@
+export class MyDate {
+  year: number;
+  month: number;
+  day: number
+  constructor(year:number,month:number,day:number){
+    this.year = year
+    this.month = month
+    this.day = day
+  }
+
+  printFormat():string{
+    return `${this.day}/${this.month}/${this.year}`
+  }
+
+
+  add(amount:number,type: 'day'| 'months' | 'years'){
+    if(type === 'day'){
+      this.day += amount
+    }
+    if(type === 'months'){
+      this.month += amount
+    }
+    if(type === 'years'){
+      this.year += amount
+    }
+  }
+}
+
+const myDate = new MyDate(1999,7,12)
+console.log(myDate.printFormat())
+myDate.add(2,'day')
+console.log(myDate.printFormat())
