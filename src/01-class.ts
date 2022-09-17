0edfb0fdb4d15bbacddd2a9da0df6581fe0114a0
Cristian Rojas <crisdarroca@hotmail.com>
2022-09-17 12:58:59 -0500

    class

---
+++ src/01-class.ts
@@ -0,0 +1,13 @@
+class MyDate {
+  year: number;
+  month: number;
+  day: number
+  constructor(year:number,month:number,day:number){
+    this.year = year
+    this.month = month
+    this.day = day
+  }
+}
+
+const myDate = new MyDate(2022,11,22)
+console.log(myDate)
