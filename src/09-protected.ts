d9012c4a1d58ba4a3a2f16b2bf0f10ba405a32b8
Cristian Rojas <crisdarroca@hotmail.com>
2022-09-23 20:32:14 -0500

    protected

---
+++ src/09-protected.ts
@@ -0,0 +1,29 @@
+export class Animal{
+  constructor(protected name: string ){}
+
+  move(){
+    console.log('Moving Along!')
+  }
+
+  greeting(){
+    return `Hello, I'm ${this.name}`
+  }
+
+}
+
+export class Dog extends Animal{
+
+  constructor(name:string,public owner:string){
+    super(name)
+  }
+
+  woof(times:number):void{
+    for(let i = 0; i <= times;i++){
+      console.log('woof!',this.name)
+    }
+  }
+}
+
+
+const lion = new Dog('Lion','David')
+lion.woof(1)
