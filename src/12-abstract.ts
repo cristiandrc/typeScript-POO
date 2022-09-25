7859ae3f8e373190b2a811142284f11692924072
Cristian Rojas <crisdarroca@hotmail.com>
2022-09-24 22:53:31 -0500

    abstract

---
+++ src/12-abstract.ts
@@ -0,0 +1,8 @@
+import { Animal,Dog } from "./09-protected";
+
+// const animal = new Animal('elite')// this shows error 
+// animal.greeting()
+
+const dog = new Dog('chid','David')
+dog.greeting()
+dog.woof(4)
